fe8ad05253d691a59912c4111e151fd7f5cd3901
James <871270605@qq.com>
2019-05-29 15:43:33 +0800

    Update index.js

--- 办公导航/js/index.js
+++ 办公导航/js/index.js
@@ -1,13 +1,16 @@
-var bangong_left_li=document.querySelectorAll(".bangong_left li")
-		var bangong_right_iframe=document.querySelector(".bangong_right_iframe")
-		   for(var i=0;i<bangong_left_li.length;i++){
-		   	      bangong_left_li[i].onmousedown=function(){
-//		   	      	$("bangong_right_iframe").load(url,"index.html")
-bangong_right_iframe.setAttribute("src",this.getAttribute("data_add"));
-		   	      	for(var j=0;j<bangong_left_li.length;j++){
-		   	      		bangong_left_li[j].removeAttribute("class")
-		   	      	}
-		   	      	this.setAttribute("class","bangone_left_fox")
-		   	      }
-		   	
-		   }
+var boxss = document.getElementsByTagName("body")[0]
+		var bangong_left = document.querySelector(".bangong_left")
+		var bangong_right_iframe = document.querySelector(".bangong_right_iframe")
+		boxss.appendChild(bangong_left);
+		boxss.appendChild(bangong_right_iframe);
+		var bangong_left_li = document.querySelectorAll(".bangong_left li")
+		for (var i = 0; i < bangong_left_li.length; i++) {
+			bangong_left_li[i].onmousedown = function() {
+				bangong_right_iframe.setAttribute("src", this.getAttribute("data_add"));
+				for (var j = 0; j < bangong_left_li.length; j++) {
+					bangong_left_li[j].removeAttribute("class")
+				}
+				this.setAttribute("class", "bangone_left_fox")
+			}
+
+		}
