8954daab01e3df8b7ec8103d6bb22e938926eb9d
James <871270605@qq.com>
2019-05-29 15:06:13 +0800

    Add files via upload

---
+++ 办公导航/js/index.js
@@ -0,0 +1,13 @@
+var bangong_left_li=document.querySelectorAll(".bangong_left li")
+		var bangong_right_iframe=document.querySelector(".bangong_right_iframe")
+		   for(var i=0;i<bangong_left_li.length;i++){
+		   	      bangong_left_li[i].onmousedown=function(){
+//		   	      	$("bangong_right_iframe").load(url,"index.html")
+bangong_right_iframe.getAttribute("src",this.getAttribute("data_add"));
+		   	      	for(var j=0;j<bangong_left_li.length;j++){
+		   	      		bangong_left_li[j].removeAttribute("class")
+		   	      	}
+		   	      	this.setAttribute("class","bangone_left_fox")
+		   	      }
+		   	
+		   }
